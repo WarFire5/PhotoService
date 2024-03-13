890459302b16b96b8abe26b03b138b79eee36f6e
Antonina66 <88768298+Antonina66@users.noreply.github.com>
2024-03-14 00:40:01 +0300

    Add mock in service

---
+++ PhotoService.BLL/Models/OutputModels/ServiceOutputModel.cs
@@ -0,0 +1,18 @@
+namespace PhotoService.BLL.Models.OutputModels;
+
+public class ServiceOutputModel
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string ExecutorName { get; set; } //Будут тянуться из списка Исполнителя
+    
+    public int Rating { get; set; } //Будут тянуться из Отзывов
+    
+    public string ServiceType  { get; set; }
+    
+    public int Cost  { get; set; }
+}
