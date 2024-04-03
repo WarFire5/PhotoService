3db72426ec6c290b7b08a6e8d875069de57d2d6a
WarFire5 <tatyanamirvoda@yandex.ru>
2024-04-04 00:06:35 +0300

    in progress

--- PhotoService.BLL/Models/InputModels/UsersInputModel.cs
+++ PhotoService.BLL/Models/InputModels/UsersInputModel.cs
@@ -1,16 +1,12 @@
-using PhotoService.DAL.DTO;
-
 namespace PhotoService.BLL.Models.InputModels;
 
 public class UsersInputModel
 {
     public int Id { get; set; }
-    public RolesDto Role { get; set; }
     public string Password { get; set; }
     public string Name { get; set; }
     public string Mail { get; set; }
     public string? Phone { get; set; }
-    public TypesDto? Specialization { get; set; }
     public string? Dossier { get; set; }
     public int? Rating { get; set; }
     public bool IsBlocked { get; set; }
@@ -18,8 +14,8 @@ public class UsersInputModel
     public string? URL { get; set; }
     public string? ExecutorType { get; set; }
     public string? CompanyTitle { get; set; }
-    public int? INN { get; set; }
-    public int? OGRN { get; set; }
+    public string? INN { get; set; }
+    public string? OGRN { get; set; }
     public bool IsDenied { get; set; }
     public string? ReasonDenied { get; set; }
 }
